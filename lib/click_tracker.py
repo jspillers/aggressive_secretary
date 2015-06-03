8b7aff60a773e971d8a655afa5a9b7f4184c06f5
Jrod <jarrod@jtms.net>
2015-06-02 23:33:09 -0400

    click tracker cycle

--- lib/click_tracker.py
+++ lib/click_tracker.py
@@ -1,50 +1,73 @@
+class ClickTrackers:
+
+    def __init__(self, _pygame, _screen, _sgc, _logger):
+        self.corp_click_track = ClickTracker(_pygame, _screen, _sgc, 'corp', 20, 20, _logger)
+        self.runner_click_track = ClickTracker(_pygame, _screen, _sgc, 'runner', 420, 20, _logger)
+        self.runner_click_track.clicks = 4
+        self.current_player = 'corp'
+        self.logger = _logger
+        self.turn_num = 1
+
+    def update(self):
+        self.corp_click_track.update()
+        self.runner_click_track.update()
+
+    def click_event(self, event):
+        self.logger.info(self.turn_num)
+        if self.current_player == 'corp':
+            if self.corp_click_track.clicks == self.corp_click_track.max_clicks:
+                self.runner_click_track.reset()
+                self.current_player = 'runner'
+        else:
+            if self.runner_click_track.clicks == self.runner_click_track.max_clicks:
+                self.corp_click_track.reset()
+                self.current_player = 'corp'
+                self.turn_num += 1
 
 class ClickTracker:
 
-    def __init__(self, _pygame, _screen, _sgc, tracker_type, x_pos, y_pos):
+    def __init__(self, _pygame, _screen, _sgc, tracker_type, x_pos, y_pos, _logger):
         self.tracker_type = tracker_type
         self.x_pos = x_pos
         self.y_pos = y_pos
         self.clicks = 0
-        self.num_clicks = 4 if tracker_type == 'runner' else 3
+        self.max_clicks = 4 if tracker_type == 'runner' else 3
+        self.click_images = []
         self.pygame = _pygame
         self.screen = _screen
         self.sgc = _sgc
+        self.logger = _logger
 
+        self.click_image_positions = [
+            (x_pos + 4, y_pos + 3), 
+            (x_pos + 79, y_pos + 46), 
+            (x_pos + 154, y_pos + 3), 
+            (x_pos + 229, y_pos + 46)
+        ]
         self.init_tracker()
     
     def init_tracker(self):
         self.tracker_bg = self.pygame.image.load(
             'images/' + self.tracker_type + '_click_track.png').convert_alpha()
-        self.tracker_bg
-        self.tracker = self.sgc.Container(
-            pos=(self.x_pos, self.y_pos),
-            widgets=self.widgets()
-        )
-        self.tracker.add()
 
-    def widgets(self):
-        _widgets = [
-            self.sgc.Simple('images/click_track_mask.png', pos=(4, 3)),
-            self.sgc.Simple('images/click_track_mask.png', pos=(79, 46)),
-            self.sgc.Simple('images/click_track_mask.png', pos=(154, 3)),
-            self.sgc.Simple('images/click_track_mask.png', pos=(229, 46))
-        ]
-        if self.tracker_type == 'runner':
-            return _widgets
-        else:
-            return _widgets[0:3]
+        self.click_btn = self.sgc.Button(label='click', pos=(self.x_pos, self.y_pos + 100))
+        self.click_btn.on_click = self.advance_click
+        self.click_btn.add()
+
+    def click_image(self):
+        return self.pygame.image.load('images/click_track_mask.png').convert_alpha()
 
     def update(self):
         self.screen.blit(self.tracker_bg, [self.x_pos, self.y_pos])
 
-    #def advance_click():
-    #    if len(self.clicks) == self.num_clicks:
-    #        self.tracker.widgets = []
-    #        self.clicks = 0
-    #    else:
-    #        self.clicks += 1
-    #        _mask = sgc.Simple('images/click_track_mask.png')
-    #        logger.info(_mask)
-    #        self.tracker.widgets.append(_mask)
+        for click in list(range(self.clicks)):
+            self.screen.blit(self.click_image(), self.click_image_positions[click])
+
+    def reset(self):
+      self.clicks = 0
+
+    def advance_click(self):
+        self.sgc.Button.on_click(self.click_btn)
+        if self.clicks < self.max_clicks:
+            self.clicks += 1
 
--- lib/secretary.py
+++ lib/secretary.py
@@ -22,30 +22,32 @@ screen = sgc.surface.Screen((800,480))
 clock = pygame.time.Clock()
 
 bg_image = BackgroundImages(pygame, screen, logger)
+click_tracks = ClickTrackers(pygame, screen, sgc, logger)
 
-corp_click_track = ClickTracker(pygame, screen, sgc, 'corp', 20, 20)
 corp_credits = IncrementableCounter(pygame, sgc, 'credits', 20, 200)
 corp_handsize = IncrementableCounter(pygame, sgc, 'corp_handsize', 200, 200)
 corp_bad_publicity = IncrementableCounter(pygame, sgc, 'bad_publicity', 20, 400, 0, 18)
 
-runner_click_track = ClickTracker(pygame, screen, sgc, 'runner', 420, 20)
 runner_credits = IncrementableCounter(pygame, sgc, 'credits', 420, 200)
 runner_handsize = IncrementableCounter(pygame, sgc, 'runner_handsize', 620, 200)
 runner_brain_damage = IncrementableCounter(pygame, sgc, 'brain_damage', 420, 400, 0, 12)
 runner_memory_units = IncrementableCounter(pygame, sgc, 'memory_units', 620, 400, 16, 25)
+runner_tags = IncrementableCounter(pygame, sgc, 'tags', 620, 300, 0, 12)
 
 while True:
     time = clock.tick(30)
 
     for event in pygame.event.get():
         sgc.event(event)
+        if event.type == GUI:
+            click_tracks.click_event(event)
+
         if event.type == QUIT:
             exit()
 
     screen.fill((0,0,0))
     bg_image.cycle_images()
-    runner_click_track.update()
-    corp_click_track.update()
+    click_tracks.update()
     sgc.update(time)
     pygame.display.flip()
 
