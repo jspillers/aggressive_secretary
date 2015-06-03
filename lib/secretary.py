ce273a28853601f57faed76597d2996e8909a2a6
Jrod <jarrod@jtms.net>
2015-06-03 11:08:41 -0400

    fullscreen

--- lib/secretary.py
+++ lib/secretary.py
@@ -34,16 +34,43 @@ runner_brain_damage = IncrementableCounter(pygame, sgc, 'brain_damage', 420, 400
 runner_memory_units = IncrementableCounter(pygame, sgc, 'memory_units', 620, 400, 16, 25)
 runner_tags = IncrementableCounter(pygame, sgc, 'tags', 620, 300, 0, 12)
 
+def toggle_fullscreen():
+    screen = pygame.display.get_surface()
+    tmp = screen.convert()
+    caption = pygame.display.get_caption()
+    cursor = pygame.mouse.get_cursor()  # Duoas 16-04-2007 
+    
+    w,h = screen.get_width(),screen.get_height()
+    flags = screen.get_flags()
+    bits = screen.get_bitsize()
+    
+    pygame.display.quit()
+    pygame.display.init()
+    
+    screen = pygame.display.set_mode((w,h),flags^FULLSCREEN,bits)
+    screen.blit(tmp,(0,0))
+    pygame.display.set_caption(*caption)
+ 
+    pygame.key.set_mods(0) #HACK: work-a-round for a SDL bug??
+ 
+    pygame.mouse.set_cursor( *cursor )  # Duoas 16-04-2007
+    
+    return screen
+
 while True:
     time = clock.tick(30)
 
-    for event in pygame.event.get():
-        sgc.event(event)
-        if event.type == GUI:
-            click_tracks.click_event(event)
+    for e in pygame.event.get():
+        sgc.event(e)
+        if e.type == GUI:
+            click_tracks.click_event(e)
 
-        if event.type == QUIT:
+        if e.type == QUIT:
             exit()
+
+        if (e.type is KEYDOWN and e.key == K_RETURN
+                and (e.mod&(KMOD_LALT|KMOD_RALT)) != 0):
+            toggle_fullscreen()
 
     screen.fill((0,0,0))
     bg_image.cycle_images()
